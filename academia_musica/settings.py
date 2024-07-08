1ce91bcc3c8ed317387b4c44af0c4323e6ed4d87
angelmaria <angelmaria75@gmail.com>
2024-07-08 18:59:24 +0200

    añado librería coreapi para la documentación

--- academia_musica/settings.py
+++ academia_musica/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'coreapi',
     'api'
 ]
 
@@ -129,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+}
