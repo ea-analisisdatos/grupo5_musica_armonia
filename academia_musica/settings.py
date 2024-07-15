f3ea704b418b1f36cc2c77044df3c61ba3c77108
AndreaMoraledaBlancom <andreamoraleda@gmail.com>
2024-07-15 13:40:20 +0200

    Show toolbar Django logs

--- academia_musica/settings.py
+++ academia_musica/settings.py
@@ -60,6 +60,25 @@ MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR': False,  # Configuración para no mostrar la barra de herramientas en desarrollo
+}
+
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
@@ -91,7 +110,7 @@ WSGI_APPLICATION = 'academia_musica.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'musica_academia',  # Nombre de tu base de datos MySQL
+        'NAME': 'musica_armonia',  # Nombre de tu base de datos MySQL
         'USER': 'Mar',
         'PASSWORD': os.getenv ('DB_PASSWORD'),
         'HOST': 'localhost',  # Cambiar si la base de datos está en otro lugar
