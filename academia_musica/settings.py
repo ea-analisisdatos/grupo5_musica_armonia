9635c60ee353c7855c7fcb4ddd93530376a50d4a
angelmaria <angelmaria75@gmail.com>
2024-07-17 09:50:13 +0200

    cambio de una comma en settings.py

--- academia_musica/settings.py
+++ academia_musica/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static"],
+    BASE_DIR / "static",]
 
 # Configuración de archivos de media
 MEDIA_URL = '/media/'
