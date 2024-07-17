8849a528691c598dffe3cb58e43fa404c21b5f15
ea-analisisdatos <erikaalvares.analisisdatos@gmail.com>
2024-07-17 21:26:32 +0200

    preparando entorno para PythonAnyWhere version Django en PythonAnyWhere es 3.10 y para djangorestframework==3.12.4. El archivo .env con los datos de acceso, llaves, contraseñas etc... debe de quedar a nivel de la carpeta manage.py

--- academia_musica/wsgi.py
+++ academia_musica/wsgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+path = os.path.expanduser('')
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'academia_musica.settings')
 
 application = get_wsgi_application()
+
+
+
+
+path = os.path.expanduser('~/grupo5_musica_armonia')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'academia_musica.settings'
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
